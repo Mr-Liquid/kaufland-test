1051320413e458fe00a1d93a2f29d5a2a3385be2
Ivan <ivan.webgroup@gmail.com>
2016-07-11 21:45:18 +0300

    finished test

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,33 @@
     "use strict";
     angular
         .module('kaufland',  ['ui.bootstrap'])
-        .controller('kauflandCtrl',function(postsService){
+        .controller('kauflandCtrl',function($scope, postsService, $uibModal){
             var vm = this;
+
             postsService.getPosts().then(function(data){
                 vm.selectData = data;
-            })
+                vm.selected = data[0].id;
+            });
+            $scope.$watch('kauf.selected',function(newVal,oldVal){
+              if(typeof newVal !== 'undefined' ){
+                postsService.getPost(newVal).then(function(data){
+                  vm.selectDataItem = data;
+                });
+              }
+            });
+          vm.open = function() {
+            $uibModal.open({
+              templateUrl: '/js/templates/modal.html',
+              controller: function($scope){
+                $scope.close = function(){
+                  this.$close();
+                };
+              },
+              scope: $scope,
+              size: 'md'
+            });
+          }
+
 
         });
 
--- public/js/services/posts.service.js
+++ public/js/services/posts.service.js
@@ -6,16 +6,36 @@
 
     angular
         .module('kaufland')
-        .factory('postsService',function($http){
-            return {
-                getPosts: _getPosts
-            };
+        .factory('postsService',function($http, $q){
+            var
+              store = null,
+              length,
+              i;
 
+            return {
+                getPosts: _getPosts,
+                getPost: _getPost
+            };
+            function _getPost(id){
+              for(var i = 0; i < store.posts.length; i++){
+                if(store.posts[i].id === id){
+                  return $q.when(store.posts[i]);
+                }
+              }
+            }
             function _getPosts(){
-               return $http.get('http://jsonplaceholder.typicode.com/posts')
-                   .then(function(res){
-                       return res.data;
-                   })
+              if(store !== null && typeof store.posts !== 'undefined'){
+                  return $q.when(store.posts);
+              }else {
+                return $http.get('http://jsonplaceholder.typicode.com/posts')
+                  .then(function(res){
+                    store = {
+                      posts:res.data
+                    };
+                    length = store.posts.length;
+                    return res.data;
+                  })
+              }
             }
         })
 
